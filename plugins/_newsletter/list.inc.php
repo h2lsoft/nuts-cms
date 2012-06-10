5b7d13772c4e237408966afac0b857c136c3528d
H2lsoft <contact@h2lsoft.com>
2012-06-10 17:52:00 +0200

    Newsletter> date FR

--- plugins/_newsletter/list.inc.php
+++ plugins/_newsletter/list.inc.php
@@ -17,7 +17,7 @@ $plugin->listSearchAddFieldText('Subject', $lang_msg[1]);
 
 // create fields
 $plugin->listAddCol('ID', '', 'center; width:30px', true);
-$plugin->listAddCol('LogActionCreateDateGMT', 'Date', '; width:30px; white-space: nowrap;', false);
+$plugin->listAddCol('Date', '', '; width:30px; white-space: nowrap;', false);
 $plugin->listAddCol('uFrom', $lang_msg[2], '; width:30px; white-space: nowrap;', true);
 $plugin->listAddCol('Subject', $lang_msg[1], '', true);
 $plugin->listAddCol('TotalSend', $lang_msg[3], 'center; width:60px; white-space: nowrap;', true);
@@ -31,7 +31,10 @@ $plugin->listRender(20, 'hookData');
 
 function hookData($row)
 {
+    global $nuts;
+
 	$row['Date'] = nutsGetGMTDateUser($row['LogActionCreateDateGMT']);
+    if($_SESSION['Language'] == 'fr')$row['Date'] = $nuts->db2Date($row['Date']);
 	
 	$total_views = (@($row['TotalViews']/$row['TotalSend']) * 100);
 	$total_views = round($total_views, 2);
