db7febe4beb778da7e630a87ae839c431840ad6b
H2lsoft <contact@h2lsoft.com>
2012-07-09 17:44:34 +0200

    init

--- nuts/_inc/session.inc.php
+++ nuts/_inc/session.inc.php
@@ -1,7 +1,6 @@
 <?php
 
 @session_start();
-
 if(isset($_COOKIE['NutsRemember']) && !empty($_COOKIE['NutsRemember']))
 {
 	$v = $_COOKIE['NutsRemember'];
@@ -37,99 +36,98 @@ if(!isset($_SESSION['NutsUserID']) || $_SESSION['NutsUserID'] == '')
 // verify record and IP
 $_SESSION['NutsUserID'] = (int)$_SESSION['NutsUserID'];
 $sql = "SELECT
-				NutsUser.ID,
-				NutsUser.Login,
+                NutsUser.ID,
+                NutsUser.Login,
 
-				NutsUser.Gender,
-				NutsUser.FirstName,
-				NutsUser.LastName,
-				NutsUser.NutsGroupID,
-				NutsUser.Timezone,
-				NutsUser.Country,
+                NutsUser.Gender,
+                NutsUser.FirstName,
+                NutsUser.LastName,
+                NutsUser.NutsGroupID,
+                NutsUser.Timezone,
+                NutsUser.Country,
 
-				NutsGroup.FrontofficeAccess,
+                NutsGroup.FrontofficeAccess,
 
-				NutsGroup.TinyMceConfig,
+                NutsGroup.TinyMceConfig,
 
-				NutsGroup.AllowUpload,
-				NutsGroup.AllowEdit,
-				NutsGroup.AllowDelete,
-				NutsGroup.AllowFolders,
+                NutsGroup.AllowUpload,
+                NutsGroup.AllowEdit,
+                NutsGroup.AllowDelete,
+                NutsGroup.AllowFolders,
 
-				Language,
-				Email
-		FROM
-				NutsUser,
-				NutsGroup
-		WHERE
-				NutsUser.NutsGroupID = NutsGroup.ID AND
-				NutsUser.ID = {$_SESSION['NutsUserID']} AND
-				NutsUser.Active = 'YES' AND
-				NutsGroup.Deleted = 'NO' AND
-				NutsGroup.BackofficeAccess = 'YES' AND
-				NutsUser.Deleted = 'NO'";
+                Language,
+                Email
+        FROM
+                NutsUser,
+                NutsGroup
+        WHERE
+                NutsUser.NutsGroupID = NutsGroup.ID AND
+                NutsUser.ID = {$_SESSION['NutsUserID']} AND
+                NutsUser.Active = 'YES' AND
+                NutsGroup.Deleted = 'NO' AND
+                NutsGroup.BackofficeAccess = 'YES' AND
+                NutsUser.Deleted = 'NO'";
 $nuts->doQuery($sql);
 if($nuts->dbNumrows() != 1)
 {
-	nutsDestroyIt();
+    nutsDestroyIt();
 }
 else
 {
-	$tmp_id = $_SESSION['NutsUserID'];
-	$last_form_percent_rID = (int)@$_SESSION['FormPercentRecordID'];
-	$serial = array();
-	if(@is_array($_SESSION['FormPercentParams']))
-	{
-		$serial = $_SESSION['FormPercentParams'];
-	}
-	
-	// preserve keys
-	$tmp_keys = array();
-	foreach($nuts_session_preserve_keys as $tmp_key)
-	{
-		if(isset($_SESSION[$tmp_key]))
-			$tmp_keys[$tmp_key] = $_SESSION[$tmp_key];
-	}
-	
-	//session_regenerate_id();
-	$_SESSION = $nuts->dbFetch();
-	$_SESSION['NutsUserID'] = $tmp_id;
-	
-	if($last_form_percent_rID)
-	{
-		$_SESSION['FormPercentRecordID'] = $last_form_percent_rID;
-		$_SESSION['FormPercentParams'] = $serial;
-	}
-	
-	foreach($tmp_keys as $tmp_key => $tmp_val)
-	{
-		$_SESSION[$tmp_key] = $tmp_val;
-	}
-	
+    $tmp_id = $_SESSION['NutsUserID'];
+    $last_form_percent_rID = (int)@$_SESSION['FormPercentRecordID'];
+    $serial = array();
+    if(@is_array($_SESSION['FormPercentParams']))
+    {
+        $serial = $_SESSION['FormPercentParams'];
+    }
 
-	// verify ip only one
-	$IP = $nuts->getIP();
+    // preserve keys
+    $tmp_keys = array();
+    foreach($nuts_session_preserve_keys as $tmp_key)
+    {
+        if(isset($_SESSION[$tmp_key]))
+            $tmp_keys[$tmp_key] = $_SESSION[$tmp_key];
+    }
 
-	$nuts->doQuery("SELECT
-							IP
-					FROM
-							NutsLog
-					WHERE
-							NutsGroupID = {$_SESSION['NutsGroupID']} AND
-							NutsUserID = {$_SESSION['ID']}
-					ORDER BY
-							ID DESC
-					LIMIT 1");
-	if($nuts->dbNumrows() == 1)
-	{
-		$sqlIP = $nuts->dbGetOne();
-		$sqlIP = long2ip($sqlIP);
-		if($sqlIP != $IP)
-		{
-			nutsDestroyIt();
-		}
-	}
+    //session_regenerate_id();
+    $_SESSION = $nuts->dbFetch();
+    $_SESSION['NutsUserID'] = $tmp_id;
+
+    if($last_form_percent_rID)
+    {
+        $_SESSION['FormPercentRecordID'] = $last_form_percent_rID;
+        $_SESSION['FormPercentParams'] = $serial;
+    }
+
+    foreach($tmp_keys as $tmp_key => $tmp_val)
+    {
+        $_SESSION[$tmp_key] = $tmp_val;
+    }
+
+    // verify ip only one
+    $IP = $nuts->getIP();
+
+    $nuts->doQuery("SELECT
+                            IP
+                    FROM
+                            NutsLog
+                    WHERE
+                            NutsGroupID = {$_SESSION['NutsGroupID']} AND
+                            NutsUserID = {$_SESSION['ID']}
+                    ORDER BY
+                            ID DESC
+                    LIMIT 1");
+    if($nuts->dbNumrows() == 1)
+    {
+        $sqlIP = $nuts->dbGetOne();
+        $sqlIP = long2ip($sqlIP);
+        if($sqlIP != $IP)
+            nutsDestroyIt();
+    }
+
 }
+
 
 
 
