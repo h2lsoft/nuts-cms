6960aaa4cc317c47ff926b3865dd27e058e75ac2
H2lsoft <contact@h2lsoft.com>
2012-07-20 18:10:43 +0200

    remove rights after delete main plugin

--- plugins/_plugins/delete.inc.php
+++ plugins/_plugins/delete.inc.php
@@ -1,7 +1,24 @@
 <?php
 
 $plugin->deleteDbTable(array('NutsMenu'));
+
+if($plugin->deleteUserHasConfirmed())
+{
+    // remove menu really
+    $sql = "DELETE FROM NutsMenu WHERE ID = {$_GET['ID']}";
+    $nuts->doQuery($sql);
+
+    // remove all rights from menu
+    $sql = "DELETE FROM NutsMenuRight WHERE NutsMenuID = {$_GET['ID']}";
+    $nuts->doQuery($sql);
+
+}
+
+
+
 $plugin->deleteRender();
+
+
 
 
 ?>
