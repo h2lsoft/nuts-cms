b3f2f78a0e96725c255682e28e60e18221befdc6
H2lsoft <contact@h2lsoft.com>
2012-10-23 14:47:30 +0200

    Urlrewriting> add a tag + remove Position + list search

--- plugins/_url_rewriting/form.inc.php
+++ plugins/_url_rewriting/form.inc.php
@@ -13,12 +13,13 @@ $plugin->formAddFieldSelect('Type', '', true, array('SIMPLE', 'REGEX'));
 $plugin->formAddFieldText('Pattern', '', true, '', '', '', '', 'Regex ex: `#/fr/thanks#i`');
 $plugin->formAddFieldText('Replacement', '', true, '', '', '', '', 'Regex replace $ by §');
 $plugin->formAddFieldText('Position', '', true, 'number', 'width:50px', '', '', 'put -1 to add at end');
+$plugin->formAddFieldTextAjaxAutoComplete('Tag', '', false);
 
 
 if($_POST)
 {
 	// form assignation
-	if($_GET['Position'] == -1)
+	if($_POST['Position'] == -1)
 	{
 		// get max position
 		$_POST['Position'] = $plugin->formGetMaxPosition('Position');
--- plugins/_url_rewriting/list.inc.php
+++ plugins/_url_rewriting/list.inc.php
@@ -11,6 +11,7 @@ $plugin->listSearchAddFieldText('ID');
 $plugin->listSearchAddFieldSelectSql('Type');
 $plugin->listSearchAddFieldText('Pattern');
 $plugin->listSearchAddFieldText('Replacement');
+$plugin->listSearchAddFieldTextAjaxAutoComplete('Tag');
 
 
 // create fields
@@ -20,6 +21,7 @@ $plugin->listAddCol('Type', '', 'center; width:30px', false); // with order by
 $plugin->listAddCol('Pattern', '', '', false); // with order by
 $plugin->listAddCol('Replacement', '', '', false);
 $plugin->listAddCol('Position', '', 'center; width:30px', true);
+$plugin->listAddCol('Tag', '', 'center; width:30px; white-space:nowrap;', false);
 
 
 // render list
@@ -30,6 +32,9 @@ function hookData($row)
 	global $plugin;
 
 	// $row['Position'] = $plugin->listGetPositionContents($row['ID']);
+    if(!empty($row['Tag']))
+        $row['Tag'] = "<span class='tag'>{$row['Tag']}</span>";
+
 	return $row;
 }
 
