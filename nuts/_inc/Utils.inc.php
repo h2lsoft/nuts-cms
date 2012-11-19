1ebb1259ae936bec5469456eb41cf11eeaf38bd2
H2lsoft <contact@h2lsoft.com>
2012-11-19 14:30:34 +0100

    + add function NutsTrigger

--- nuts/_inc/Utils.inc.php
+++ nuts/_inc/Utils.inc.php
@@ -373,8 +373,40 @@ function nutsUserGetPassword($NutsUserID)
 	
 }
 
+/**
+ * Allow to start/auto registered trigger
+ *
+ * @param $name
+ * @param bool $auto_register (false)
+ * @param string $description
+ */
+function nutsTrigger($name, $auto_register=false, $description="")
+{
+	global $nuts;
 
+    $sql = "SELECT * FROM NutsTrigger WHERE Deleted = 'NO' AND Name = '".sqlX($name)."' LIMIT 1";
+    $nuts->doQuery($sql);
+    if(!$nuts->dbNumRows())
+    {
+        if($auto_register)
+        {
+            $f = array();
+            $f['Name'] = $name;
+            $f['Description'] = $description;
+            $nuts->dbInsert('NutsTrigger', $f);
+        }
+    }
+    else
+    {
+        $rec = $nuts->dbFetch();
+        $rec['PhpCode'] = trim($rec['PhpCode']);
+        if(!empty($rec['PhpCode']))
+        {
+            eval($rec['PhpCode']);
+        }
+    }
 
+}
 
 
 
