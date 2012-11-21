245cadefd4babd400ea437a12ee80ebcc0ff4d12
H2lsoft <contact@h2lsoft.com>
2012-11-21 13:37:41 +0100

    + default url_rewriting

--- nuts/url_rewriting_rules.inc.php
+++ nuts/url_rewriting_rules.inc.php
@@ -16,11 +16,20 @@ $uri_str_replaces[] = "/en/11.html";
 $uri_str_patterns[] = "/en/my_account/";
 $uri_str_replaces[] = "/en/12.html";
 
+$uri_str_patterns[] = "/en/my_profile/";
+$uri_str_replaces[] = "/en/145.html";
+
 
 
 // regex replacements *************************
 $uri_patterns = array();
 $uri_replaces = array();
+
+$uri_patterns[] = "#^/en/news/$#";
+$uri_replaces[] = "/en/14.html";
+
+$uri_patterns[] = "#/en/news/(.*).html#";
+$uri_replaces[] = "/en/15.html";
 
 
 ?>
