d7d4a973f04209db91d7c13566d230c443da0a6d
H2lsoft <contact@h2lsoft.com>
2012-11-21 12:59:49 +0100

    remove accent

--- install/after_FR.sql
+++ install/after_FR.sql
@@ -1,7 +1,7 @@
 UPDATE NutsUser SET `Language` = 'fr';
 UPDATE NutsNews SET `Language` = 'fr', VirtualPageName = REPLACE(VirtualPageName, '/en/', '/fr/');
 UPDATE NutsPage SET `Language` = 'fr';
-UPDATE NutsTemplateConfiguration SET LanguageDefault = 'fr', Description = 'Thème par défaut';
+UPDATE NutsTemplateConfiguration SET LanguageDefault = 'fr', Description = 'Theme par défaut';
 UPDATE NutsUrlRewriting SET Pattern = REPLACE(Pattern, '/en/', '/fr/'), Replacement = REPLACE(Replacement, '/en/', '/fr/');
 
 TRUNCATE TABLE NutsForm;
@@ -9,7 +9,7 @@ insert into `NutsForm` (`ID`, `Language`, `Name`, `Description`, `Caption`, `Css
 
 TRUNCATE TABLE NutsFormField;
 INSERT INTO `NutsFormField`(`ID`,`NutsFormID`,`Name`,`Label`,`Type`,`Required`,`Attributes`,`Email`,`OtherValidation`,`I18N`,`Value`,`PhpCode`,`FilePath`,`FileAllowedExtensions`,`FileAllowedMimes`,`FileMaxSize`,`HtmlCode`,`TextAfter`,`Position`,`Deleted`) VALUES
-	(1,1,'Company','Société','TEXT','NO','','NO','','NO','','','','','','','','',1,'NO'),
+	(1,1,'Company','Societe','TEXT','NO','','NO','','NO','','','','','','','','',1,'NO'),
 	(2,1,'Name','Votre nom','TEXT','YES','','NO','','NO','','','','','','','','',2,'NO'),
 	(4,1,'Email','Votre email','TEXT','YES','','YES','','NO','','','','','','','','',3,'NO'),
 	(5,1,'Message','Votre message','TEXTAREA','YES','','NO','','NO','','','','','','','','',4,'NO');
