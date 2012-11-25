dc6a78a6ba7bf4c6d49a9b03bc3eb97f4fd5855a
H2lsoft <contact@h2lsoft.com>
2012-11-25 18:50:23 +0100

    cfg updates

--- plugins/_press-kit/config.inc.php
+++ plugins/_press-kit/config.inc.php
@@ -7,6 +7,7 @@ $plugins_lng['en']['date_format'] = "%Y/%m/%d"; // in sql mode with %
 $plugins_lng['en']['source'] = "Source / Author";
 $plugins_lng['en']['title'] = "Title";
 $plugins_lng['en']['date'] = "Date";
+$plugins_lng['en']['size'] = "Size";
 $plugins_lng['en']['file'] = "";
 
 
@@ -17,10 +18,11 @@ $plugins_lng['fr']['date_format'] = "%d/%m/%Y"; // in sql mode with %
 $plugins_lng['fr']['source'] = "Source / Auteur";
 $plugins_lng['fr']['title'] = "Titre";
 $plugins_lng['fr']['date'] = "Date";
+$plugins_lng['fr']['size'] = "Taille";
 $plugins_lng['fr']['file'] = "";
 
 
-$include_plugin_css = true; // inlude bundle css dynamically
+$include_plugin_css = true; // include bundle css dynamically
 
 
 
