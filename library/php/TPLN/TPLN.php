00f402741493680fe99efef124db987ab1614c69
H2lsoft <contact@h2lsoft.com>
2012-11-25 16:50:24 +0100

    Comment> clickable url add rel=nofollow

--- library/php/TPLN/TPLN.php
+++ library/php/TPLN/TPLN.php
@@ -2751,9 +2751,10 @@ class TPLN extends DB
 	 * This method allows to convert http, www, ftp, mailto into clickable element
 	 *
 	 * @param string $ret
+	 * @param boolean $add_nofollow
 	 * @return string
 	 * @author H2LSOFT */
-	public function clickable($ret)
+	public function clickable($ret, $add_nofollow=true)
 	{
 		/*$str = eregi_replace("([_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)+)",
 							   "<a href=\"mailto:\\1\">\\1</a>", $str);
@@ -2764,11 +2765,12 @@ class TPLN extends DB
 		// matches an "xxxx://yyyy" URL at the start of a line, or after a space.
 		// xxxx can only be alpha characters.
 		// yyyy is anything up to the first space, newline, comma, double quote or <
-		$ret = preg_replace("#(^|[\n ])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $ret);
+        $nofollow = ($add_nofollow) ? 'rel="nofollow"' : '';
+		$ret = preg_replace("#(^|[\n ])([\w]+?://[\w\#$%&~/.\-;:=,?@\[\]+]*)#is", "\\1<a href=\"\\2\" $nofollow target=\"_blank\">\\2</a>", $ret);
 
 		// matches an email@domain type address at the start of a line, or after a space.
 		// Note: Only the followed chars are valid; alphanums, "-", "_" and or ".".
-		$ret = preg_replace("#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\">\\2@\\3</a>", $ret);
+		$ret = preg_replace("#(^|[\n ])([a-z0-9&\-_.]+?)@([\w\-]+\.([\w\-\.]+\.)*[\w]+)#i", "\\1<a href=\"mailto:\\2@\\3\" $nofollow>\\2@\\3</a>", $ret);
 
 		// matches a "www|ftp.xxxx.yyyy[/zzzz]" kinda lazy URL thing
 		// Must contain at least 2 dots. xxxx contains either alphanum, or "-"
