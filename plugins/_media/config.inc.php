3002f7a6e186722bacc90eb55b5785f0a58acad3
H2lsoft <contact@h2lsoft.com>
2012-12-16 23:19:06 +0100

    default video

--- plugins/_media/config.inc.php
+++ plugins/_media/config.inc.php
@@ -1,7 +1,7 @@
 <?php
 
-$video_default_width = 320;
-$video_default_height = 240;
+$video_default_width = 640;
+$video_default_height = 360;
 $video_default_top = ''; // assign default url for a logo by example url|x|y
 $video_default_skin = ''; // assign default skin, use url of jpg file
 
