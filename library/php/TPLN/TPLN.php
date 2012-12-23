568b89ff519b5dcc7c527def5cc5f82919fde6b7
H2lsoft <contact@h2lsoft.com>
2012-12-23 19:06:19 +0100

    add referer on mail alert

--- library/php/TPLN/TPLN.php
+++ library/php/TPLN/TPLN.php
@@ -478,14 +478,16 @@ class TPLN extends DB
 
 			$body = date('[Y-m-d H:i] ')." TPLN has detected an error\n\n";
 			$body .= $err_msg.' in '.$_SERVER['SCRIPT_FILENAME']."\n\n\n";
-			$body .= "Url <a href=\"$url\">$url</a>\n";
+			$body .= "<b>Url :</b> <a href=\"$url\">$url</a>\n";
 
-			$body .= "===========================================\n";
-			$body .= 'TPLN version '.TPLN_VERSION."\n";
-			$body .= 'IP: '.$this->GetIP()." (<a href=\"http://www.geoiptool.com/en/?IP=".$this->GetIP()."\">information</a>)"."\n";
-			$body .= "Server: {$_SERVER['SERVER_NAME']} ({$_SERVER['SERVER_ADDR']})"."\n";
-            $referer = (isset($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : '';
-			$body .= "Referer: $referer"."\n";
+			// $body .= "===========================================\n";
+            $body .= "<hr>";
+			$body .= '<b>TPLN version '.TPLN_VERSION."</b>\n";
+			$body .= '<b>IP :</b> '.$this->GetIP()." (<a href=\"http://www.geoiptool.com/en/?IP=".$this->GetIP()."\">information</a>)"."\n";
+			$body .= "<b>Server :</b> {$_SERVER['SERVER_NAME']} ({$_SERVER['SERVER_ADDR']})"."\n";
+            $referer = (isset($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : '-';
+			$body .= "<b>Referer :</b> $referer"."\n";
+			$body .= "<b>Agent :</b> ".@$_SERVER['HTTP_USER_AGENT']."\n";
 
 
 			$body = str_replace("\n", "<br />", $body);
