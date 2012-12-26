ba23d5f54583233627cee5d9ed8cc9f51e83fe60
H2lsoft <contact@h2lsoft.com>
2012-12-26 16:09:29 +0100

    add method Browser information

--- library/php/TPLN/TPLN.php
+++ library/php/TPLN/TPLN.php
@@ -192,6 +192,111 @@ class TPLN extends DB
 		return;
 	}
 
+
+    /**
+     * Get browser information
+     * @param string $u_agent (if empty user $_SERVER['HTTP_USER_AGENT'])
+     * @return array
+     */
+    public function getBrowserInfo($u_agent="")
+    {
+        if(empty($u_agent))$u_agent = $_SERVER['HTTP_USER_AGENT'];
+
+        $bname = 'Unknown';
+        $platform = 'Unknown';
+        $version = "";
+
+        //First get the platform?
+        if (preg_match('/linux/i', $u_agent)) {
+            $platform = 'Linux';
+        }
+        elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+            $platform = 'Mac';
+        }
+        elseif (preg_match('/Windows 95|Win95|Windows_95/i', $u_agent))$platform = 'Windows 95';
+        elseif (preg_match('/Windows 98|Win98/i', $u_agent))$platform = 'Windows 98';
+        elseif (preg_match('/Windows NT 5.0|Windows 2000/i', $u_agent))$platform = 'Windows 2000';
+        elseif (preg_match('/Windows NT 5.1|Windows xp/i', $u_agent))$platform = 'Windows XP';
+        elseif (preg_match('/Windows NT 5.2/i', $u_agent))$platform = 'Windows Server 2003';
+        elseif (preg_match('/Windows NT 5.2|Windows NT 6.0/i', $u_agent))$platform = 'Windows Vista';
+        elseif (preg_match('/Windows NT 6.1/i', $u_agent))$platform = 'Windows 7';
+        elseif (preg_match('/Windows NT 6.2/i', $u_agent))$platform = 'Windows 8';
+
+        elseif (preg_match('/windows|win32/i', $u_agent)) {
+            $platform = 'Windows';
+        }
+
+        // Next get the name of the useragent yes seperately and for good reason
+        if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent))
+        {
+            $bname = 'Internet Explorer';
+            $ub = "MSIE";
+        }
+        elseif(preg_match('/Firefox/i',$u_agent))
+        {
+            $bname = 'Mozilla Firefox';
+            $ub = "Firefox";
+        }
+        elseif(preg_match('/Chrome/i',$u_agent))
+        {
+            $bname = 'Google Chrome';
+            $ub = "Chrome";
+        }
+        elseif(preg_match('/Safari/i',$u_agent))
+        {
+            $bname = 'Apple Safari';
+            $ub = "Safari";
+        }
+        elseif(preg_match('/Opera/i',$u_agent))
+        {
+            $bname = 'Opera';
+            $ub = "Opera";
+        }
+        elseif(preg_match('/Netscape/i',$u_agent))
+        {
+            $bname = 'Netscape';
+            $ub = "Netscape";
+        }
+
+        // finally get the correct version number
+        $known = array('Version', $ub, 'other');
+        $pattern = '#(?<browser>' . join('|', $known) .
+            ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+        if (!preg_match_all($pattern, $u_agent, $matches)) {
+            // we have no matching number just continue
+        }
+
+        // see how many we have
+        $i = count($matches['browser']);
+        if ($i != 1) {
+            //we will have two since we are not using 'other' argument yet
+            //see if version is before or after the name
+            if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+                $version= $matches['version'][0];
+            }
+            else {
+                $version= $matches['version'][1];
+            }
+        }
+        else {
+            $version= $matches['version'][0];
+        }
+
+        // check if we have a number
+        if($version==null || $version=="") {$version="?";}
+
+        return array(
+            'userAgent' => $u_agent,
+            'name'      => $bname,
+            'version'   => $version,
+            'platform'  => $platform,
+            'pattern'    => $pattern
+        );
+
+    }
+
+
+
 	/**
 	 * This method returns ip used.
 	 *
