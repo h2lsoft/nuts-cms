6808872c681f202cee4ff0c791bb96a800e42346
H2lsoft <contact@h2lsoft.com>
2012-12-31 13:47:04 +0100

    ups

--- plugins/_social-share/www/index.inc.php
+++ plugins/_social-share/www/index.inc.php
@@ -18,6 +18,18 @@ $plugin->openPluginTemplate();
 
 $label = ($page->language == 'fr') ? 'Partager' : 'Share';
 $plugin->parse('label', $label);
+
+
+$title = $page->vars['H1'];
+if(empty($title))$title = $page->vars['MetaTitle'];
+if(empty($title))$title = $page->vars['MenuName'];
+
+$plugin->parse('title', $title);
+$plugin->parse('titleX', urlencode($title));
+
+
+
+
 $plugin->setNutsContent();
 
 
