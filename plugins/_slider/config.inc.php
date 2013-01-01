152b8ef960dbf2d39e91d9a609f97cb39eeda0e5
H2lsoft <contact@h2lsoft.com>
2013-01-01 21:11:36 +0100

    init

---
+++ plugins/_slider/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin slider - configuration
+ *
+ * @version 1.0
+ * @date 01/01/2013
+ * @author H2lsoft (contact@h2lsoft.com) - http://www.h2lsoft.com
+ */
+
+/* @var $plugin Plugin */
+/* @var $nuts NutsCore */
+
+$include_plugin_css = true; // inlude bundle css dynamically
+$include_plugin_js = true; // inlude bundle js dynamically
+
+
+
+?>
---
+++ plugins/_slider/lang/fr.inc.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin slider - Language file
+ * 
+ * @version 1.0
+ * @date 01/01/2013
+ * @author H2lsoft (contact@h2lsoft.com) - http://www.h2lsoft.com
+ */
+
+$lang_msg[0] = "Slider Images"; // plugin name
+
+$lang_msg[1] = "Nom";
+$lang_msg[2] = "Ajouter à la fin `px` ou `%`";
+$lang_msg[3] = "";
+
+
+
+
+?>
