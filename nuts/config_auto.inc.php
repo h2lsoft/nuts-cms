a5d5255a687457c9c52d309f5baaa9caaf0c49ee
H2lsoft <contact@h2lsoft.com>
2013-03-24 19:02:59 +0100

    add apps path

--- nuts/config_auto.inc.php
+++ nuts/config_auto.inc.php
@@ -6,7 +6,26 @@ define('NUTS_URL', WEBSITE_URL.'/nuts');
 
 // library
 define('NUTS_LIBRARY_PATH', WEBSITE_PATH.'/library');
-define('NUTS_LIBRARY_URL', WEBSITE_URL.'/library');
+define('NUTS_LIBRARY_URL', '/library');
+
+// app
+define('NUTS_APPS_PATH', WEBSITE_PATH.'/app');
+define('NUTS_APPS_URL', '/app');
+
+
+// uploads
+define('NUTS_UPLOADS_PATH', WEBSITE_PATH.'/uploads');
+define('NUTS_UPLOADS_URL', '/uploads');
+
+
+// plugins
+define('NUTS_PLUGINS_PATH', WEBSITE_PATH.'/plugins');
+define('NUTS_PLUGINS_URL', '/plugins');
+
+
+// themes
+define('NUTS_THEMES_PATH', WEBSITE_PATH.'/themes');
+define('NUTS_THEMES_URL', '/themes');
 
 // php
 define('NUTS_PHP_PATH', NUTS_LIBRARY_PATH.'/php');
@@ -39,20 +58,6 @@ define('NUTS_GALLERY_IMAGES_HD_URL', NUTS_IMAGES_URL.'/gallery_images_hd');
 define('NUTS_NEWS_IMAGES_PATH', NUTS_IMAGES_PATH.'/news');
 define('NUTS_NEWS_IMAGES_URL', NUTS_IMAGES_URL.'/news');
 
-
-// themes
-define('NUTS_THEMES_PATH', NUTS_LIBRARY_PATH.'/themes');
-define('NUTS_THEMES_URL', NUTS_LIBRARY_URL.'/themes');
-
-
-// uploads
-define('NUTS_UPLOADS_PATH', WEBSITE_PATH.'/uploads');
-define('NUTS_UPLOADS_URL', WEBSITE_URL.'/uploads');
-
-
-// plugins
-define('NUTS_PLUGINS_PATH', WEBSITE_PATH.'/plugins');
-define('NUTS_PLUGINS_URL', WEBSITE_URL.'/plugins');
 
 
 $nutsBackLogo = (BACKOFFICE_LOGO_URL == '') ? 'img/logo.png' : BACKOFFICE_LOGO_URL;
