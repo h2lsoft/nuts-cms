8dba710d2f5aea23317d22ef35edbaf1d1a7b834
H2lsoft <contact@h2lsoft.com>
2013-04-16 14:28:51 +0200

    Search> --delete-after parameter for wget

--- plugins/_search-engine/lang/fr.inc.php
+++ plugins/_search-engine/lang/fr.inc.php
@@ -4,7 +4,7 @@ $lang_msg[0] = "Moteur de recherche"; // plugin name
 
 $lang_msg[1] = "Configurer le plugin";
 $lang_msg[2] = "Lancer le script de recherche, <a href=\"[SITEMAP_COMPLETE_URL]\" target=\"blank\">ici</a>";
-$lang_msg[3] = "Configurer votre tâche planifiée <pre style=\"border:1px solid #ccc; background-color:#e5e5e5; padding:5px;\">Linux: `wget [SITEMAP_COMPLETE_URL] 2>&1 >/dev/null` </pre>";
+$lang_msg[3] = "Configurer votre tâche planifiée <pre style=\"border:1px solid #ccc; background-color:#e5e5e5; padding:5px;\">Linux: `wget  --delete-after [SITEMAP_COMPLETE_URL]` </pre>";
 
 
 ?>
