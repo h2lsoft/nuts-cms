61c06f0c936c5b9e32d87f1b30f2dfbdabeba87b
H2lsoft <contact@h2lsoft.com>
2013-04-16 14:36:22 +0200

    _Email> no white space for group

--- plugins/_email/list.inc.php
+++ plugins/_email/list.inc.php
@@ -15,7 +15,7 @@ $plugin->listSearchAddFieldText('Subject', $lang_msg[2]);
 // create fields
 $plugin->listAddCol('ID', '', 'center; width:30px', true, array());
 $plugin->listAddColImg('Language', $lang_msg[1], '', true, NUTS_IMAGES_URL.'/flag/{Language}.gif');
-$plugin->listAddCol('GroupName', $lang_msg[7], ';width:30px', true, array());
+$plugin->listAddCol('GroupName', $lang_msg[7], ';width:30px; white-space:nowrap;', true, array());
 $plugin->listAddCol('Description', $lang_msg[8], '', false, array());
 $plugin->listAddCol('Expeditor', $lang_msg[4], 'center; width:30px; white-space:nowrap;', false, array());
 $plugin->listAddCol('Subject', $lang_msg[2], '', false, array());
