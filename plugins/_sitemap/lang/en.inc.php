d9ab1883b37626e12aa8bd717f53fc7d2ceb6392
H2lsoft <contact@h2lsoft.com>
2013-04-16 14:30:14 +0200

    add --delete-after parameter for wget

--- plugins/_sitemap/lang/en.inc.php
+++ plugins/_sitemap/lang/en.inc.php
@@ -4,7 +4,7 @@ $lang_msg[0] = "Sitemap generator"; // plugin name
 
 $lang_msg[1] = "Configure plugin";
 $lang_msg[2] = "Launch sitemap generator, <a href=\"[SITEMAP_COMPLETE_URL]\" target=\"blank\">here</a>";
-$lang_msg[3] = "Configure your scheduler with <pre style=\"border:1px solid #ccc; background-color:#e5e5e5; padding:5px;\">Linux: `wget [SITEMAP_COMPLETE_URL] 2>&1 >/dev/null` </pre>";
+$lang_msg[3] = "Configure your scheduler with <pre style=\"border:1px solid #ccc; background-color:#e5e5e5; padding:5px;\">Linux: `wget --delete-after [SITEMAP_COMPLETE_URL]` </pre>";
 $lang_msg[4] = "Add this line in your robots.txt file, <a href=\"[CONTROL_CENTER_URL]\" target=\"blank\">here</a> <pre style=\"border:1px solid #ccc; background-color:#e5e5e5; padding:5px;\">Sitemap: [SITEMAP_COMPLETE_URL]</pre>";
 
 
