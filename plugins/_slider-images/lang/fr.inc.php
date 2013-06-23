6989fd53372345bf9483f1f57ebea3312a8698ee
H2lsoft <contact@h2lsoft.com>
2013-06-23 20:43:04 +0200

    translation FR

--- plugins/_slider-images/lang/fr.inc.php
+++ plugins/_slider-images/lang/fr.inc.php
@@ -7,7 +7,7 @@
  * @author H2lsoft (contact@h2lsoft.com) - http://www.h2lsoft.com
  */
 
-$lang_msg[0] = "Slider Images"; // plugin name
+$lang_msg[0] = "Images de votre slider"; // plugin name
 
 $lang_msg[1] = "Titre";
 
