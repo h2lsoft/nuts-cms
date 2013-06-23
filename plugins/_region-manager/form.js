07b8272b4a74f96bd69d01072c3aa0eaf336d992
H2lsoft <contact@h2lsoft.com>
2013-06-23 20:10:13 +0200

    improve js toggle option pager

--- plugins/_region-manager/form.js
+++ plugins/_region-manager/form.js
@@ -7,25 +7,59 @@ initCodeEditor('HtmlNoRecord', 'html', true);
 initCodeEditor('HookData', 'php', true);
 
 
+
 function updatePager()
 {
 	v = $('#Pager').val();
 	if(v == 'YES')
 	{
-		$('#PagerPreviousText').parents('p').show();
-		$('#PagerNextText').parents('p').show();
+        $('#SetUrl').parents('p').show();
+
+        $('#PreviousStartEndVisible').parents('p').show();
+        $('#PagerStartText').parents('p').show();
+        $('#PagerEndText').parents('p').show();
+
+        $('#PagerPreviousText').parents('p').show();
+        $('#PagerNextText').parents('p').show();
+
+        updatePagerStartEnd()
 	}
 	else
 	{
+		$('#SetUrl').parents('p').hide();
+
+		$('#PreviousStartEndVisible').parents('p').hide();
+        $('#PagerStartText').parents('p').hide();
+		$('#PagerEndText').parents('p').hide();
+
 		$('#PagerPreviousText').parents('p').hide();
 		$('#PagerNextText').parents('p').hide();
 	}
+}
+
+function updatePagerStartEnd()
+{
+    v = $('#PreviousStartEndVisible').val();
+    if(v == 'YES')
+    {
+        $('#PagerStartText').parents('p').show();
+        $('#PagerEndText').parents('p').show();
+    }
+    else
+    {
+        $('#PagerStartText').parents('p').hide();
+        $('#PagerEndText').parents('p').hide();
+    }
 }
 
 
 updatePager();
 $('#Pager').change(function(){
 	updatePager();
-})
+});
+
+$('#PreviousStartEndVisible').change(function(){
+    updatePagerStartEnd();
+});
 
 
