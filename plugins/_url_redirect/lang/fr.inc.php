1e66a69aeaa897470668f870f5549d1c999e3055
H2lsoft <contact@h2lsoft.com>
2013-06-23 20:29:56 +0200

    translation FR

---
+++ plugins/_url_redirect/lang/fr.inc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin url_redirect - Language file
+ * 
+ * @version 1.0
+ * @date 23/06/2013
+ * @author H2lsoft (contact@h2lsoft.com) - http://www.h2lsoft.com
+ */
+
+$lang_msg[0] = "Url redirection"; // plugin name
+
+$lang_msg[1] = "Nouvelle url";
+$lang_msg[2] = "Ecrire .htaccess";
+$lang_msg[3] = "Ancienne url";
+$lang_msg[4] = "Nouvelle url";
+
+
+?>
