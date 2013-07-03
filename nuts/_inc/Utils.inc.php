46222380790d40cdd8e0dc27e0b6fe633a90d4bc
H2lsoft <contact@h2lsoft.com>
2013-07-03 19:41:59 +0200

    add nuts cache functions

--- nuts/_inc/Utils.inc.php
+++ nuts/_inc/Utils.inc.php
@@ -787,4 +787,70 @@ EOF;
     return $str;
 }
 
+
+/**
+ * Get application cache (delete expiration before)
+ *
+ * @param $app
+ *
+ * @return mixed boolean|string
+ */
+function nutsGetCache($app)
+{
+	global $nuts;
+
+	nutsClearCache($app);
+
+	Query::factory()->select('Content')
+				    ->from('NutsCache')
+					->whereEqualTo('Application', $app)
+					->order_by('Date DESC')
+					->limit(1)
+					->execute();
+
+	if($nuts->dbNumRows() == 0)
+		return false;
+	else
+		return $nuts->dbGetOne();
+
+}
+
+/**
+ * Clear cache application automatic with NutsGetCache
+ */
+function nutsClearCache($app)
+{
+	global $nuts;
+
+	$sql = "DELETE FROM NutsCache WHERE Expiration <= NOW() AND Application = '".sqlX($app)."'";
+	$nuts->doQuery($sql);
+}
+
+/**
+ * Create a cacche application
+ *
+ * @param $app
+ * @param $expiration (sql mode YYYY-MM-DD HH:II:SS)
+ */
+function nutsSetCache($app, $expiration)
+{
+	global $nuts;
+
+	$f = array();
+	$f['Date'] = 'NOW()';
+	$f['Application'] = $app;
+	$f['Expiration'] = $expiration;
+
+	$nuts->dbInsert('NutsCache', $f);
+
+}
+
+
+
+
+
+
+
+
+
 ?>
