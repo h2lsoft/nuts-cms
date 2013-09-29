c3567938b07ed7f74aaf95a5c5ea39bbfa857100
H2lsoft <contact@h2lsoft.com>
2013-09-29 17:12:59 +0200

    en translation

--- plugins/_comments/lang/en.inc.php
+++ plugins/_comments/lang/en.inc.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin comments - Language file
- * 
+ *
  * @version 1.0
  * @date 31/12/2012
  * @author H2lsoft (contact@h2lsoft.com) - http://www.h2lsoft.com
@@ -12,10 +12,8 @@ $lang_msg[0] = "Comments"; // plugin name
 $lang_msg[1] = "Name";
 
 $lang_msg[2] = "Set comments visible";
-$lang_msg[3] = "Set comments Hidden";
+$lang_msg[3] = "Set comments hidden";
 $lang_msg[4] = "Delete comments";
-
-
 
 
 ?>
