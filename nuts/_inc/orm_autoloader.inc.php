5feb22c98d777b3ad513eb977f7ea583061302fe
H2lsoft <contact@h2lsoft.com>
2014-03-05 15:10:09 +0100

    add conditional inclusion

--- nuts/_inc/orm_autoloader.inc.php
+++ nuts/_inc/orm_autoloader.inc.php
@@ -1,6 +1,8 @@
 <?php
 
 function orm_autoloader($class) {
-	include WEBSITE_PATH.'/x_includes/orm/'.$class.'.db.class.php';
+
+	if(file_exists(WEBSITE_PATH.'/x_includes/orm/'.$class.'.db.class.php'))
+		include WEBSITE_PATH.'/x_includes/orm/'.$class.'.db.class.php';
 }
 spl_autoload_register('orm_autoloader');
