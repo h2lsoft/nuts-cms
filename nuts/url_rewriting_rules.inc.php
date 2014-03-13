5ae1c835ea0cf576fc0fa99de0e3dd8780789a90
H2lsoft <contact@h2lsoft.com>
2014-03-13 12:38:57 +0100

    reformat

--- nuts/url_rewriting_rules.inc.php
+++ nuts/url_rewriting_rules.inc.php
@@ -1,35 +1,18 @@
 <?php
 
 // simples replacements *************************
-$uri_str_patterns = array();
-$uri_str_replaces = array();
-
-$uri_str_patterns[] = "/en/access_restricted/";
-$uri_str_replaces[] = "/en/13.html";
-
-$uri_str_patterns[] = "/en/login/";
-$uri_str_replaces[] = "/en/10.html";
-
-$uri_str_patterns[] = "/en/register/";
-$uri_str_replaces[] = "/en/11.html";
-
-$uri_str_patterns[] = "/en/my_account/";
-$uri_str_replaces[] = "/en/12.html";
-
-$uri_str_patterns[] = "/en/my_profile/";
-$uri_str_replaces[] = "/en/145.html";
-
+$uri_str_patterns = array(); $uri_str_replaces = array();
+$uri_str_patterns[] = "/en/access_restricted/"; $uri_str_replaces[] = "/en/13.html";
+$uri_str_patterns[] = "/en/login/"; $uri_str_replaces[] = "/en/10.html";
+$uri_str_patterns[] = "/en/register/"; $uri_str_replaces[] = "/en/11.html";
+$uri_str_patterns[] = "/en/about/"; $uri_str_replaces[] = "/en/1.html";
+$uri_str_patterns[] = "/en/gallery/"; $uri_str_replaces[] = "/en/3.html";
+$uri_str_patterns[] = "/en/faq/"; $uri_str_replaces[] = "/en/173.html";
+$uri_str_patterns[] = "/en/my_account/"; $uri_str_replaces[] = "/en/12.html";
+$uri_str_patterns[] = "/en/my_profile/"; $uri_str_replaces[] = "/en/145.html";
 
 
 // regex replacements *************************
-$uri_patterns = array();
-$uri_replaces = array();
-
-$uri_patterns[] = "#^/en/news/$#";
-$uri_replaces[] = "/en/14.html";
-
-$uri_patterns[] = "#/en/news/(.*).html#";
-$uri_replaces[] = "/en/15.html";
-
-
-?>
+$uri_patterns = array(); $uri_replaces = array();
+$uri_patterns[] = "#^/en/news/$#"; $uri_replaces[] = "/en/14.html";
+$uri_patterns[] = "#/en/news/(.*).html#"; $uri_replaces[] = "/en/15.html";
